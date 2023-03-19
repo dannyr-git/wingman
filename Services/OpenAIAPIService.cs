b788754460715dd022cfa25b4e17b22d202a8013
Danny Rodriguez <danny.rodriguez@outlook.com>
2023-03-19 11:29:21 -0400

    Added Whisper API pre-prompt

--- Services/OpenAIAPIService.cs
+++ Services/OpenAIAPIService.cs
@@ -163,8 +163,11 @@ namespace wingman.Services
             var fileBytes = await ReadFileBytes(inmp3);
 
             Logger.LogDebug("Sending audio to Whisper API");
+            var sult = new AudioCreateTranscriptionRequest;
+            //sult.Prompt
             var completionResult = await _openAIService.Audio.CreateTranscription(new AudioCreateTranscriptionRequest
             {
+                Prompt = "The following transcription will be contextually based around programming, coding, or scripting.  It might include technical language and programming terminology.",
                 Model = Models.WhisperV1,
                 ResponseFormat = StaticValues.AudioStatics.ResponseFormat.Text,
                 File = fileBytes,
